0b08721e9644230fc47a9d8d59e41e1d2f1fd383
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2021-01-20 09:03:51 +0000

    add new values for settings

--- templates/settings.php
+++ templates/settings.php
@@ -12,12 +12,20 @@
     <div id="sciencemeshAddrSettings">
         <p><?php p($l->t("Site Name")) ?></p>
         <p><input id="sciencemeshSitename" value="<?php p($_["sitename"]) ?>" placeholder="CERN" type="text"></p>
+        <p><?php p($l->t("Site URL")) ?></p>
+        <p><input id="sciencemeshSiteurl" value="<?php p($_["siteurl"]) ?>" placeholder="http://localhost" type="text"></p>
         <p><?php p($l->t("Country Code")) ?></p>
         <p><input id="sciencemeshCountryCode" value="<?php p($_["country"]) ?>" placeholder="CH" type="text"></p>
         <p><?php p($l->t("Hostname")) ?></p>
         <p><input id="sciencemeshHostname" value="<?php p($_["hostname"]) ?>" placeholder="example.org/xcloud/" type="text"></p>
         <p><?php p($l->t("IOP Service Address")) ?></p>
         <p><input id="sciencemeshIopUrl" value="<?php p($_["iopurl"]) ?>" placeholder="http://<IOP URL>/" type="text"></p>
+        <p><?php p($l->t("Number of users")) ?></p>
+        <p><input id="sciencemeshNumusers" value="<?php p($_["numusers"]) ?>" placeholder="0" type="number"></p>
+        <p><?php p($l->t("Number of files")) ?></p>
+        <p><input id="sciencemeshNumfiles" value="<?php p($_["numfiles"]) ?>" placeholder="0" type="number"></p>
+        <p><?php p($l->t("Storage volume (in bytes)")) ?></p>
+        <p><input id="sciencemeshNumstorage" value="<?php p($_["numstorage"]) ?>" placeholder="0" type="number"></p>
     </div>
 
     <div>
