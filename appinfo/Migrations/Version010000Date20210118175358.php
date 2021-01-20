e7a7a43f555692fcfcc74bd0a07fb85c38ce5303
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2021-01-20 10:16:01 +0000

    migration step

--- appinfo/Migrations/Version010000Date20210118175358.php
+++ appinfo/Migrations/Version010000Date20210118175358.php
@@ -1,89 +1,65 @@
 <?php
 
-declare(strict_types=1);
+namespace OCA\ScienceMesh\Migrations;
 
-namespace OCA\ScienceMesh\Migration;
-
-use Doctrine\DBAL\Schema\Schema;
 use OCP\Migration\ISchemaMigration;
-use Closure;
-use OCP\DB\ISchemaWrapper;
-use OCP\Migration\IOutput;
-use Doctrine\DBAL\Types\Type;
-use OCP\Migration\SimpleMigrationStep;
+use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration step: Please modify to your needs!
+ * Create initial tables for the app
  */
-class Version010000Date20210118175358 extends SimpleMigrationStep {
+class Version010000Date20210118175358 implements ISchemaMigration {
 
-	/**
-	 * @param IOutput $output
-	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
-	 * @param array $options
-	 */
-	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
-	}
+    /** @var  string */
+    private $prefix;
 
-	/**
-	 * @param IOutput $output
-	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
-	 * @param array $options
-	 * @return null|ISchemaWrapper
-	 */
-	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
-		/** @var ISchemaWrapper $schema */
-		$schema = $schemaClosure();
+    /**
+     - @param Schema $schema
+     - @param [] $options
+     */
+    public function changeSchema(Schema $schema, array $options) {
+        $this->prefix = $options['tablePrefix'];
 
-		if (!$schema->hasTable('sciencemesh')) {
-			$table = $schema->createTable('sciencemesh');
-			$table->addColumn('iopurl', 'string', [
-				'notnull' => true,
-				'default' => 'http://localhost:10999',
-			]);
-			$table->addColumn('country', 'string', [
-				'notnull' => true,
-				'length' => 2,
-			]);
-			$table->addColumn('hostname', 'string', [
-				'notnull' => true,
-				'default' => 'example.org',
-			]);
-			$table->addColumn('sitename', 'string', [
-				'notnull' => true,
-				'default' => 'CERNBox',
-			]);
-			$table->addColumn('siteurl', 'string', [
-				'notnull' => true,
-				'default' => 'http://localhost',
-			]);
-			$table->addColumn('numusers', Type::BIGINT, [
-				'notnull' => true,
-				'notnull' => false,
-				'default' => 0,
-				'unsigned' => true,
-			]);
-			$table->addColumn('numfiles', Type::BIGINT, [
-				'notnull' => true,
-				'notnull' => false,
-				'default' => 0,
-				'unsigned' => true,
-			]);
-			$table->addColumn('numstorage', Type::BIGINT, [
-				'notnull' => true,
-				'notnull' => false,
-				'default' => 0,
-				'unsigned' => true,
-			]);
-		}
-		return $schema;
-	}
-
-	/**
-	 * @param IOutput $output
-	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
-	 * @param array $options
-	 */
-	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
-	}
+        if (!$schema->hasTable("{$this->prefix}sciencemesh")) {
+            $table = $schema->createTable("{$this->prefix}sciencemesh");
+            $table->addColumn('iopurl', 'string', [
+              'notnull' => true,
+              'default' => 'http://localhost:10999',
+            ]);
+            $table->addColumn('country', 'string', [
+              'notnull' => true,
+              'length' => 2,
+            ]);
+            $table->addColumn('hostname', 'string', [
+              'notnull' => true,
+              'default' => 'example.org',
+            ]);
+            $table->addColumn('sitename', 'string', [
+              'notnull' => true,
+              'default' => 'CERNBox',
+            ]);
+            $table->addColumn('siteurl', 'string', [
+              'notnull' => true,
+              'default' => 'http://localhost',
+            ]);
+            $table->addColumn('numusers', Type::BIGINT, [
+              'notnull' => true,
+              'notnull' => false,
+              'default' => 0,
+              'unsigned' => true,
+            ]);
+            $table->addColumn('numfiles', Type::BIGINT, [
+              'notnull' => true,
+              'notnull' => false,
+              'default' => 0,
+              'unsigned' => true,
+            ]);
+            $table->addColumn('numstorage', Type::BIGINT, [
+              'notnull' => true,
+              'notnull' => false,
+              'default' => 0,
+              'unsigned' => true,
+            ]);
+        }
+    }
 }
